4f8ee8c39abd2a9f1ee599be948e5858250b86b2
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-05-27 20:05:24 +0000

    issues displaying images

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,15 @@
+class ImagesController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @course = Course.find(params[:course_id])
+        @course.images.create(image_params.merge(user: current_user))
+        redirect_to course_path(@course)
+    end 
+
+    private
+
+    def image_params
+        params.require(:image).permit(:picture, :caption)
+    end
+end
