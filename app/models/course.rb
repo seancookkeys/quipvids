9ae53070a476f5be4d7774ef44546922b471936a
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-05-28 03:01:45 +0000

    added stripe and payment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+    mount_uploader :image, ImageUploader 
+
     belongs_to :user
     has_many :sections
     has_many :images
@@ -7,5 +9,12 @@ class Course < ApplicationRecord
     validates :title, presence: true 
     validates :description, presence: true 
     validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0} 
-    mount_uploader :image, ImageUploader 
+    
+    def free?
+        cost.zero?
+    end
+
+    def premium?
+        ! free?
+    end
 end
