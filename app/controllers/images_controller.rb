4729621d9255543ea3efae30d64fdecc2aed93a8
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-06-17 03:42:15 +0000

    what you changed

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
 
     def create
         @course = Course.find(params[:course_id])
